23cbb346776872d70d8e188f13a0ff92f3c735ea
AdityaZulkarnaen <adityazulkarnaen083@gmail.com>
2025-04-13 15:44:11 +0700

    add cake

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
 import './App.css'
+import CakePage from './components/CakePage'
 
 function App() {
   return (
     <>
-      
+      <CakePage/>
     </>
   )
 }
---
+++ src/components/CakePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import cake from '../assets/cake.svg'
+
+const CakePage = () => {
+  return (
+    <>
+        <div>
+            <img src={cake} className='size-96'></img>
+        </div>
+    </>
+  )
+}
+
+export default CakePage
